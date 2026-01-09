294750e901844bdd250325035aac3f0641aa3ee1
RambabuN <rambabun@SulekhaITs-MacBook-Pro.local>
2026-01-09 12:59:22 +0530

    main js issue fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/Mobile-App-Developer-Portfolio/"> {/* <-- set your repo name */}
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+  import { createRoot } from "react-dom/client";
+  import App from "./app/App.tsx";
+  import "./styles/index.css";
+
+  createRoot(document.getElementById("root")!).render(<App />);
+  
